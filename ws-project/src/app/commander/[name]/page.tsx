d88cd2a07300477c6dd12076a755c51cfc2522b2
floda69 <floflojourda@gmail.com>
2024-12-14 03:05:33 +0100

    affichage correct de page data + add logo

--- ws-project/src/app/commander/[name]/page.tsx
+++ ws-project/src/app/commander/[name]/page.tsx
@@ -3,6 +3,7 @@
 import { useParams } from 'next/navigation';
 import React from 'react';
 import {PersonPage} from '../../../Components/Templates/PersonPage';
+import NavBar from "@/Components/Templates/NavBar";
 
 export default function CommanderRoot() {
 //   const params = useParams();
@@ -13,8 +14,11 @@ export default function CommanderRoot() {
 //   }
 
   return (
-    <div className="h-screen overflow-hidden p-12 bg-cover bg-[url('https://upload.wikimedia.org/wikipedia/commons/thumb/0/02/La_Libert%C3%A9_guidant_le_peuple_-_Eug%C3%A8ne_Delacroix_-_Mus%C3%A9e_du_Louvre_Peintures_RF_129_-_apr%C3%A8s_restauration_2024.jpg/1024px-La_Libert%C3%A9_guidant_le_peuple_-_Eug%C3%A8ne_Delacroix_-_Mus%C3%A9e_du_Louvre_Peintures_RF_129_-_apr%C3%A8s_restauration_2024.jpg')]">
-      <PersonPage/>
+    <div className="h-screen overflow-hidden bg-cover bg-[url('https://upload.wikimedia.org/wikipedia/commons/thumb/0/02/La_Libert%C3%A9_guidant_le_peuple_-_Eug%C3%A8ne_Delacroix_-_Mus%C3%A9e_du_Louvre_Peintures_RF_129_-_apr%C3%A8s_restauration_2024.jpg/1024px-La_Libert%C3%A9_guidant_le_peuple_-_Eug%C3%A8ne_Delacroix_-_Mus%C3%A9e_du_Louvre_Peintures_RF_129_-_apr%C3%A8s_restauration_2024.jpg')]">
+      <NavBar/>
+      <div className="p-12 pb-20 h-full">
+        <PersonPage/>
+      </div>
     </div>
   );
 }
--- ws-project/src/app/country/[name]/page.tsx
+++ ws-project/src/app/country/[name]/page.tsx
@@ -2,6 +2,7 @@
 
 import { useParams } from 'next/navigation';
 import React from 'react';
+import NavBar from "@/Components/Templates/NavBar";
 
 export default function CountryRoot() {
 //   const params = useParams();
@@ -12,9 +13,7 @@ export default function CountryRoot() {
 //   }
 
   return (
-    <div className="h-full">
-      <h1>Country Page</h1>
-      <p>Country Name: </p>
+    <div className="h-screen overflow-hidden p-12 bg-cover bg-[url('https://upload.wikimedia.org/wikipedia/commons/thumb/0/02/La_Libert%C3%A9_guidant_le_peuple_-_Eug%C3%A8ne_Delacroix_-_Mus%C3%A9e_du_Louvre_Peintures_RF_129_-_apr%C3%A8s_restauration_2024.jpg/1024px-La_Libert%C3%A9_guidant_le_peuple_-_Eug%C3%A8ne_Delacroix_-_Mus%C3%A9e_du_Louvre_Peintures_RF_129_-_apr%C3%A8s_restauration_2024.jpg')]">
     </div>
   );
 }
