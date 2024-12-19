8e9c8be4994a7b5ffbe4c5133e4e477ced1203c4
armynion <army.brunet@laposte.net>
2024-12-19 17:53:19 +0100

    research only if option checked

--- ws-project/src/Components/Organisms/SearchArea.tsx
+++ ws-project/src/Components/Organisms/SearchArea.tsx
@@ -10,40 +10,48 @@ interface Suggestion {
   image: string;
 }
 
-
-
 export const SearchArea = () => {
-
   const [suggestions, setSuggestions] = useState<{ [key: string]: Suggestion[] }>({});
 
   const [searchConflict, setSearchConflict] = useState<boolean>(true);
   const [searchPersonalities, setSearchPersonalities] = useState<boolean>(true);
-  const [searchWar, setSearchWar] = useState<boolean>(true);
   const [searchCountries, setSearchCountries] = useState<boolean>(true);
 
   const [query, setQuery] = React.useState("");
 
   useEffect(() => {
     const search = async () => {
-      const warData = await fetchSearchWar(query);
-      const personalityData = await fetchSearchPersonality(query);
-      setSuggestions({
-        "Guerre": warData,
-        "Commandants": personalityData,
-        "Pays": [
+      const newSuggestions: { [key: string]: Suggestion[] } = {};
+
+      // Effectuer les requêtes conditionnellement en fonction des cases cochées
+      if (searchConflict) {
+        const warData = await fetchSearchWar(query);
+        newSuggestions["Guerre"] = warData;
+      }
+
+      if (searchPersonalities) {
+        const personalityData = await fetchSearchPersonality(query);
+        newSuggestions["Commandants"] = personalityData;
+      }
+
+      if (searchCountries) {
+        newSuggestions["Pays"] = [
           { label: "France", image: "https://upload.wikimedia.org/wikipedia/commons/thumb/b/b7/Armoiries_r%C3%A9publique_fran%C3%A7aise.svg/132px-Armoiries_r%C3%A9publique_fran%C3%A7aise.svg.png" },
           { label: "Allemagne", image: "https://upload.wikimedia.org/wikipedia/commons/thumb/d/da/Coat_of_arms_of_Germany.svg/132px-Coat_of_arms_of_Germany.svg.png" },
           { label: "Italie", image: "https://upload.wikimedia.org/wikipedia/commons/thumb/0/00/Emblem_of_Italy.svg/132px-Emblem_of_Italy.svg.png" },
           { label: "Espagne", image: "https://upload.wikimedia.org/wikipedia/commons/thumb/8/85/Escudo_de_Espa%C3%B1a_%28mazonado%29.svg/132px-Escudo_de_Espa%C3%B1a_%28mazonado%29.svg.png" }
-        ]
-      });
-      console.log(suggestions);
-      console.log(warData);
-      console.log(personalityData);
-    };
-    search();
+        ];
+      }
 
-  }, [query]);
+      setSuggestions(newSuggestions);
+      console.log(newSuggestions);
+    };
+
+    // Exécuter `search` uniquement si une case est cochée
+    if (query) {
+      search();
+    }
+  }, [query, searchConflict, searchPersonalities, searchCountries]);
 
   return (
     <div
@@ -57,20 +65,14 @@ export const SearchArea = () => {
       }}
     >
       <div className="flex space-x-4 mb-4 mt-10">
-        <Checkbox name="conflicts" checked={searchConflict} setChecked={setSearchConflict} label='Bataille' />
+        <Checkbox name="conflicts" checked={searchConflict} setChecked={setSearchConflict} label='Conflit' />
         <Checkbox name="personalities" checked={searchPersonalities} setChecked={setSearchPersonalities} label='Personnalités' />
-        <Checkbox name="armedForces" checked={searchWar} setChecked={setSearchWar} label='Guerre' />
         <Checkbox name="countries" checked={searchCountries} setChecked={setSearchCountries} label='Pays' />
       </div>
 
       <SearchBar onSearch={(query: string) => console.log(query)} suggestions={suggestions} query={query} setQuery={setQuery} />
-
-
     </div>
-
   );
-
-
 };
 
 export default SearchArea;
