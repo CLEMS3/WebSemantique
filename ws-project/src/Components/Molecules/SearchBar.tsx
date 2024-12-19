c773f036b4fae2d36293cf37c5a4e7f5d428a280
CLEMS3 <56449459+CLEMS3@users.noreply.github.com>
2024-12-19 15:27:47 +0100

    front de la page d'accueil

--- ws-project/src/Components/Molecules/SearchBar.tsx
+++ ws-project/src/Components/Molecules/SearchBar.tsx
@@ -97,7 +97,7 @@ const SearchBar: React.FC<SearchBarProps> = ({ onSearch, suggestions = [], query
             backgroundColor: "white",
             border: "1px solid #ccc",
             borderRadius: "0 0 4px 4px",
-            maxHeight: "200px",
+            maxHeight: "290px",
             width: "90%",
             overflowY: "auto",
             margin: 0,
